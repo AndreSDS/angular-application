f1dc60a31b7b4fc387d9fd308d18ebcac03f152e
AndreSDS <andre_smiths@outlook.com>
2023-08-30 15:05:43 -0300

    creating page home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
+const RowsHigh: {
+  [key: string]: number
+} = {
+  '1': 400,
+  '3': 335,
+  '4': 350,
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
   cols = 3;
+  rowHigh = RowsHigh[this.cols.toString()];
+  category: string | undefined;
 
   constructor() { }
 
@@ -14,6 +23,11 @@ export class HomeComponent {
 
   onColumnsCountChange(colsNumber: number): void {
     this.cols = colsNumber;
+    this.rowHigh = RowsHigh[this.cols.toString()];
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 
 }
