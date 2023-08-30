919a6a2c51afd8cafc9f54a335ed6f78b428b7aa
AndreSDS <andre_smiths@outlook.com>
2023-08-30 15:03:52 -0300

    refactoring product-header component

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -3,7 +3,6 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-products-header',
   templateUrl: './products-header.component.html',
-  styleUrls: ['./products-header.component.scss']
 })
 export class ProductsHeaderComponent {
   @Output() columnsCountChange = new EventEmitter<number>();
